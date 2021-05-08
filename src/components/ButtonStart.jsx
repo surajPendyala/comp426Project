89e2acb83779105b4da2dcd9c527f4eb23a08658
surajPendyala <68168170+surajPendyala@users.noreply.github.com>
2021-05-08 13:48:56 -0400

    Update ButtonStart.jsx

--- src/components/ButtonStart.jsx
+++ src/components/ButtonStart.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ButtonStart = (props) => {
 
     return (
-        <button className="waves-effect waves-light btn-large deep-purple lighten-2"
+        <button className=""
                 onClick={props.start}>Start Game
         </button>
     );
